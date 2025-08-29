4834c049ce025ce33d9ff07d1b7a7b04c5439f16
Akhil-Kishore <85286700+Akhil-Kishore@users.noreply.github.com>
2025-08-29 21:27:53 +0530

    feat: scaffold full stack app

---
+++ client/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+  ],
+  devServer: {
+    proxy: {
+      '/api': 'http://localhost:5000',
+    },
+  },
+};
